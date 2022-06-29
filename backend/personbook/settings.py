a039e4741627bdd48893ac58de2fd8d1845784df
Dominik Gross <dominik.gross@incibit.at>
2022-06-29 15:28:10 +0200

    add android emulator host to allowed hosts

--- backend/personbook/settings.py
+++ backend/personbook/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-$vc(4*9p42)=giu+f6-l=kt8-&*u_w^+5ud+y_qduhas+mjr%)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["10.0.2.2"] 
 
 
 # Application definition
